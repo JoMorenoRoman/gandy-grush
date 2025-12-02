7fa2459381562f25094c62934e08aca4ceb03c78
Facundo Grasso <facundo.grasso@bridgenext.com>
2025-12-02 01:38:15 -0300

    Documentar y pasar a espaniol pt.1

--- texto.py
+++ texto.py
@@ -1,27 +1,74 @@
 import os
 import pygame
-
 import config
 
 fonts:list[pygame.font.Font] = []
 
 def normal(texto:str):
+    """
+    Renderiza un texto con el estilo normal.
+
+    Args:
+        texto (str): Texto a renderizar.
+
+    Returns:
+        tuple[pygame.Surface, pygame.Rect]: Superficie con el texto renderizado y su rectángulo asociado.
+    """
     return render(texto, 0, config.TEXT_COLOR)
 
 def subtitulo(texto:str):
+    """
+    Renderiza un texto con el estilo subtítulo.
+
+    Args:
+        texto (str): Texto a renderizar.
+
+    Returns:
+        tuple[pygame.Surface, pygame.Rect]: Superficie con el texto renderizado y su rectángulo asociado.
+    """
     return render(texto, 1, config.TEXT_COLOR)
 
 def titulo(texto:str):
+    """
+    Renderiza un texto con el estilo 'título'.
+
+    Args:
+        texto (str): Texto a renderizar.
+
+    Returns:
+        tuple[pygame.Surface, pygame.Rect]: Superficie con el texto renderizado y su rectángulo asociado.
+    """
     return render(texto, 2, config.TEXT_COLOR)
     
 def render(texto:str, index:int, color:tuple[int, int, int]):
+    """
+    Renderiza un texto usando una fuente específica y color.
+
+    Args:
+        texto (str): Texto a renderizar.
+        index (int): Índice de la fuente en la lista global `fonts`.
+        color (tuple[int, int, int]): Color del texto en formato RGB.
+
+    Returns:
+        tuple[pygame.Surface, pygame.Rect]: Superficie con el texto renderizado y su rectángulo asociado.
+    """
     surf = fonts[index].render(texto, True, color)
     return (surf, surf.get_rect())
 
 def reset():
+    """
+    Reinicia la configuración de los textos llamando a la funcion `init()`.
+    """
     init()
 
 def init():
+    """
+    Inicializa la lista de fuentes `fonts` con diferentes tamaños.
+
+        - Calcula el tamaño base en función de la altura de la pantalla (`config.screen.get_height()`).
+        - Carga la fuente 'alagard.ttf' desde la carpeta 'data'.
+        - Agrega tres tamaños de fuente: normal, subtítulo y título.
+    """
     fonts.clear()
     size = int(config.screen.get_height() / 20)
     alagard = os.path.join("data", "alagard.ttf")
--- timer.py
+++ timer.py
@@ -1,5 +1,15 @@
 import config
 
+"""
+    Convierte segundos en una cantidad de frames según los fotogramas configurados.
+
+    Args:
+        amount (float): Cantidad de tiempo en segundos.
+
+    Returns:
+        int: Número entero de frames correspondientes a la duración indicada.
+"""
+
 def seconds(amount:float) -> int:
     frames = config.framerate * amount
     if frames > int(frames):
